476fc272b82e07c125fcc04c433c066eb80e021a
Bill Back <bill.back@thoughtspot.com>
2019-10-02 05:04:50 -0700

    Restructing and updating for Python 3.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ddl_tools',
+    version='1.0',
+    python_requires='>3.6',
+    description='This package contains tools for working with ThoughtSpot DDL.',
+    long_description_content_type="text/markdown",
+    url='https://thoughtspot.com',
+    author='Bill Back',
+    author_email='bill.back@thoughtspot.com',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'xlrd',
+        'openpyxl'
+    ]
+)
